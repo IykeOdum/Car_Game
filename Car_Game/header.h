94fc3c2e0226ddd82bc4bf06af2ba206ca335dff
Iyke Odum <iyke.odum@wsu.edu>
2025-04-19 14:33:35 -0700

    added sfml

---
+++ Car_Game/header.h
@@ -0,0 +1,26 @@
+/*
+
+Names: Iyke Odum, Austin Daniels, Kadara Tandia
+Assignment: Programming Assignment 9
+Due: 4/23/2025
+Description: For this final assignment, we are required to create a solution, as a team, 4
+to a game or graphical application of your choice! 
+
+*/
+
+//gaurd code
+#pragma once
+
+//libraries
+#include <iostream>
+
+
+
+//using statements
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+
+
